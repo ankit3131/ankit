1553c03c8ac9fa5415c058a3fd8a0042324accdc
ankit3131 <83747038+ankit3131@users.noreply.github.com>
2022-09-19 11:45:26 +0530

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from "react";
+import "./styles.css";
+import ImageUpload from "./ImageUpload";
+
+export default function App() {
+  return (
+    <div className="App">
+      <h1>Open file select dialogue on div tag click using React JS</h1>
+      <ImageUpload />
+    </div>
+  );
+}
